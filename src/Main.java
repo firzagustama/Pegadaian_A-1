e960a97ea7a91837c34b92d1595671948333a1a8
bimosetyo-git <57379273+bimosetyo-git@users.noreply.github.com>
2019-11-05 15:59:48 +0700

    looping inventori
    perulaangan untuk menampilkan invenorti

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static Main main = new Main();
+	
+	public static void main(String[] args) {
 	// write your code here
         List<Barang> barangList = new ArrayList<>();
 
@@ -11,6 +13,8 @@ public class Main {
         barangList.add(new Barang(1, "Firza", "Laptop", "Cepat Sekali", 7000000));
         barangList.add(new Barang(2, "Ruben", "Emas", "Berat Sekali", 10000000));
         barangList.add(new Barang(3, "Onggo", "Motor", "Bensin Habis", 3000000));
+        
+        main.inventory(barangList);
     }
 
     // TODO buat insert barang gadai disini
@@ -25,6 +29,18 @@ public class Main {
 
     // TODO buat tampilkan inventory disini
     public static void inventory(List<Barang> barangList) {
+    	//ArrayList<Barang> bl = new ArrayList<>();
+    	
+    	/*System.out.println("+----+------------+-----------------+--------------+-----------+");
+        System.out.format("| %-2s | %-10s | %-15s | %-12s | %-9s |%n", "ID", "Product", "Name", "Deskripsi", "Price");
+        System.out.println("+----+------------+-----------------+--------------+-----------+");*/
 
+        String format = "| %-2d | %-10s | %-15s | %-6s | %-9d |%n";
+        for(Barang barang : barangList) {
+        	if(barang.getStatus().equalsIgnoreCase("lunas")) {
+        		System.out.format(format, barang.getId(),barang.getNama() ,barang.getKategori(),barang.getDeskripsi() ,barang.getHarga());
+        	}
+        }
+        System.out.println("+----+------------+-----------------+--------------+-----------+");
     }
 }
