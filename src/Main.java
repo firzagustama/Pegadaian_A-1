51669d9ed5f28c1a194d4349c329bb2b362330a4
ludya3011 <57378877+ludya3011@users.noreply.github.com>
2019-11-05 16:52:08 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,21 +17,38 @@ public class Main {
     // TODO buat insert barang gadai disini
     public static void gadai(List<Barang> barangList) {
     	Scanner in = new Scanner(System.in);
+    	
+    	//inisiasi nilai
     	String nama;
     	String kategori, deskripsi;
     	int id, harga;
-    	System.out.println("Nama: ");
     	
+    	//input nama
+    	System.out.println("Nama: ");
     	do {
     		nama = in.nextLine();
     		if(nama.length()<3 || nama.length()>15) {
     			System.out.println("Masukkan Salah! Nama harus terdiri dari 3-15 karakter. Masukkan kembali. \nNama:");
     		}
     	}while (nama.length()<3 || nama.length()>15);
-    	kategori = in.nextLine();
+    	
+    	//input Kategori
+    	System.out.println("Kategori Produk: ");
+     	
+    	do {
+    		kategori = in.nextLine();
+    		if(!kategori.equalsIgnoreCase("Motor") && !kategori.equalsIgnoreCase("Laptop") && !kategori.equalsIgnoreCase("Emas")) {
+    			System.out.println("Masukkan Salah! Produk hanya terdiri dari Motor, Laptop, Emas. Masukkan kembali. \nKategori Produk:");
+    		}
+    	}while (!kategori.equalsIgnoreCase("Motor") && !kategori.equalsIgnoreCase("Laptop") && !kategori.equalsIgnoreCase("Emas"));
+    	
+    	//input deskripsi
     	deskripsi = in.nextLine();
+    	
+    	//input harga dan id
     	harga = in.nextInt();
     	id = barangList.size()+1;
+    	
     	barangList.add(new Barang(id, nama, kategori, deskripsi, harga));
     }
 
